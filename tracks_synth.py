18b7d62780d62961b439fc5562c9e5475837cd71
FinFET <76776190+FinFetChannel@users.noreply.github.com>
2021-09-11 19:41:24 -0300

    Update tracks_synth.py

--- tracks_synth.py
+++ tracks_synth.py
@@ -1,14 +1,18 @@
 import pygame as pg
 import numpy as np
 
-def synth_sine(frames, freq):
-    arr = np.sin(2*np.pi*freq*np.linspace(0,frames/44100,frames))
-##    arr = arr**3 # triangularish waves
-##    arr = arr*2 # sawtoothish wawes (more like bats)
-    arr = np.clip(100*arr, -1, 1) # squarish waves
-    fade = list(np.ones(int(frames*3/4))) +list(np.linspace(1, 0, frames-int(frames*3/4))**2)
-    arr = 32767*np.multiply(arr, np.asarray(fade))
-    return arr
+def synth(frequency, duration=1.5, sampling_rate=41000):
+    frames = int(duration*sampling_rate)
+    arr = np.cos(2*np.pi*frequency*np.linspace(0,duration, frames))
+##    arr = arr + np.cos(4*np.pi*frequency*np.linspace(0,duration, frames)) # organ like
+##    arr = arr + np.cos(6*np.pi*frequency*np.linspace(0,duration, frames)) # organ like
+    arr = np.clip(arr*10, -1, 1) # squarish waves
+##    arr = np.cumsum(np.clip(arr*10, -1, 1)) # triangularish waves pt1
+##    arr = arr+np.sin(2*np.pi*frequency*np.linspace(0,duration, frames)) # triangularish waves pt1
+##    arr = arr/max(np.abs(arr)) # adjust to -1, 1 range
+    fade = list(np.ones(frames-4100))+list(np.linspace(1, 0, 4100))
+    arr = np.multiply(arr, np.asarray(fade))
+    return list(arr)
 
 pg.init()
 pg.mixer.init()
@@ -17,42 +21,36 @@ a_file = open("noteslist.txt")
 file_contents = a_file.read(); a_file.close()
 noteslist = file_contents.splitlines()
 freq = 16.3516 #starting frequency
-frequencies = []
+freqs = {}
 
 for i in range(len(noteslist)):
-    frequencies.append(freq)
+    freqs[noteslist[i]]= freq
     freq = freq * 2 ** (1/12)
 
 with open("SuperMario.txt", "r") as file:
-    keypresses = [eval(line.rstrip()) for line in file]
+    notes = [eval(line.rstrip()) for line in file]
 file.close()
 
 track = []
-for ii in range(int(len(keypresses)/2)):
-    i = ii*2
-    freq = frequencies[noteslist.index(keypresses[i][1])]
-    track = track + list(np.zeros(max(0, int(44.1*(keypresses[i][2]-100)))))
-    track = track + list(synth_sine(int(44.1*(keypresses[i+1][2]+100)), freq))
-
-arr = np.asarray(track)
+for i in range(int(len(notes)/2)):
+    track = track + list(np.zeros(max(0, int(44.1*(notes[i*2][2]-100)))))
+    track = track + synth(freqs[notes[i*2][1]], 1e-3*(notes[i*2+1][2]+100))
+   
+arr = 32767*np.asarray(track)*0.5
 sound = np.asarray([arr,arr]).T.astype(np.int16)
 sound = pg.sndarray.make_sound(sound.copy())
 
 sound.play()
+pg.time.wait(int(len(arr)/44.1))
 
 import wave
-sfile = wave.open('mario.wav', 'w')
 
-# set the parameters
+sfile = wave.open('mario.wav', 'w')
 sfile.setframerate(44100)
 sfile.setnchannels(2)
 sfile.setsampwidth(2)
-
-# write raw PyGame sound buffer to wave file
 sfile.writeframesraw(sound)
-
-# close file
 sfile.close()
 
-pg.time.wait(int(len(arr)/44.1))
+pg.mixer.quit()
 pg.quit()    
